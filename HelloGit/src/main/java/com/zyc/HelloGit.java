da4a9186060a2909bed4bc17dc4c33c4d2385757
zycupc <zycupc@163.com>
2020-05-22 19:05:49 +0800

    创建分支

--- HelloGit/src/main/java/com/zyc/HelloGit.java
+++ HelloGit/src/main/java/com/zyc/HelloGit.java
@@ -20,7 +20,7 @@ package com.zyc;
  */
 public class HelloGit {
     public static void main(String[] args) {
-        System.out.println("第一个分支");
         System.out.println("第一次提交");
+        System.out.println("第一个分支");
     }
 }
