0e01d1e5e144c8330918def54f6b37ac98f47d1c
zycupc <zycupc@163.com>
2020-05-22 08:09:12 +0800

    第一次提交

---
+++ HelloGit/src/main/java/com/zyc/HelloGit.java
@@ -0,0 +1,25 @@
+/**
+ * Copyright (C), 2015-2020, XXX有限公司
+ * FileName: HelloGit
+ * Author:   Administrator
+ * Date:     2020/5/22 8:07
+ * Description:
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+package com.zyc;
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈〉
+ *
+ * @author Administrator
+ * @create 2020/5/22
+ * @since 1.0.0
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("第一次提交");
+    }
+}
